20fb185ee1509939114ba67db27df03a6b81df62
nguyenviet3057 <nguyenviet3057@gmail.com>
2023-02-01 11:14:23 +0700

    update code

--- EWSweb/asgi.py
+++ EWSweb/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'EWSweb.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', '999.settings')
 
 application = get_asgi_application()
